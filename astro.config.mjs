0353eb60ed39b1088f4d148fb557822bfce4ec39
Rodrigo Frazão <41315902+RodrasF@users.noreply.github.com>
2024-12-19 21:53:54 +0000

    chore: update CNAME and site configuration to reflect new domain

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,6 @@ import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://rodrasf.github.io",
+  site: "https://rodrigofrazao.dev",
   integrations: [mdx(), sitemap()],
 });
